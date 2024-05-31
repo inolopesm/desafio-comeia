a2b91c7521cafc345920ade76b309b32c3ebf959
Matheus Lopes <inolopesm@gmail.com>
2024-05-31 08:49:06 -0300

    implement graceful shutdown

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
 import { app } from "./app";
 
-app.listen(3000);
+const server = app.listen(3000);
+
+["SIGINT", "SIGTERM"].map((event) =>
+  process.on(event, () => {
+    if (!server) return;
+
+    server.close((err) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+
+      process.exit(0);
+    });
+  })
+);
