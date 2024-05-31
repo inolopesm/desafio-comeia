27fbe8914aecaf4e832e90ebbd098dc92c27a82a
Matheus Lopes <inolopesm@gmail.com>
2024-05-31 08:53:36 -0300

    customize not found route

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.all("*", (request, response, next) => {
+  response.status(404);
+  response.send({ message: "Route not found" });
+});
+
 export { app };
